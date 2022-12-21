47bbcb7909d9ef64e79b9bba5824abc6b1167d12
DIEGO FLORES ALCANTAR <diegoedvflores03@gmail.com>
2022-12-21 01:18:06 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component, useState } from 'react'
+import { NavBar } from './components/NavBar/NavBar'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+function App() {
+
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CardWidget/CardWidget.jsx
@@ -0,0 +1,11 @@
+// import 'bootstrap-icons/font/bootstrap-icons.css'
+
+
+export const CardWidget = () => {
+
+  return(
+    <div className="iconMyCar">
+      <i className="bi bi-cart3 iconCar"></i>
+    </div>
+  )
+}
