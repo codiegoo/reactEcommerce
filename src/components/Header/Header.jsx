cb986ce8805b7d79ad512930f6528441ec179068
DIEGO FLORES ALCANTAR <diegoedvflores03@gmail.com>
2023-02-16 20:46:56 -0700

    newBanner

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,6 +8,7 @@ export const Header = () => {
       <div className='bannerImg'>
         <h2 className="titleText">Devices<span>Shop</span></h2>
         <p>El ecommerce <br></br> con dispositivos <br></br> mas <span id="typedWord">seguros</span></p>
+        <img src="https://firebasestorage.googleapis.com/v0/b/reactecommercediego.appspot.com/o/objetos.png?alt=media&token=d50aef29-5e23-449a-8585-058dfb9272f8"/>
       </div>
     </header>
   )
